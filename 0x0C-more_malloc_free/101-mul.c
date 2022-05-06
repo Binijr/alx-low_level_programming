8242c276d96c7eee79b1ade1d9064c047c3de669
binijr <binijr1997@gmail.com>
2022-05-06 08:27:13 -0700

    Task o6

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -14,24 +14,23 @@ void _is_zero(char *argv[])
 	int i, isn1 = 1, isn2 = 1;
 
 	for (i = 0; argv[1][i]; i++)
-		if (argv[1][i] != '0'
+		if (argv[1][i] != '0')
 		{
-		isn1 = 0;
-		break;
+			isn1 = 0;
+			break;
 		}
-
-		for (i = 0; argv[2][i]; i++)
+	for (i = 0; argv[2][i]; i++)
 		if (argv[2][i] != '0')
 		{
-		isn2 = 0;
-		break;
+			isn2 = 0;
+			break;
 		}
 
-		if (isn1 == 1 || isn2 == 1)
-		{
+	if (isn1 == 1 || isn2 == 1)
+	{
 		printf("0\n");
 		exit(0);
-		}
+	}
 }
 
 /**
